dff89082a6041f5a8513ed7fff7deb1ed68a69cb
FSilvaFlores <franco2208silva@gmail.com>
2023-08-22 12:29:46 -0500

    update

--- Shoping-clothes/src/App.jsx
+++ Shoping-clothes/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Navbar, Container } from 'react-bootstrap';
+import ContextProvider from './Context/Context';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ContextProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-blue-700 text-black px-4 py-2 rounded" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -28,7 +32,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+
+      <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>My App</Navbar.Brand>
+      </Navbar>
+      <Button variant="primary" className="bg-blue-500 text-white">Haz clic</Button>
+
+    </Container>
+
+    </ContextProvider>
   )
 }
 

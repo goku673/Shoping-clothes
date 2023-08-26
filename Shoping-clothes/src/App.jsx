4472629d0a45a6da32916458087ea4e2664c1ab0
FSilvaFlores <franco2208silva@gmail.com>
2023-08-26 00:06:45 -0500

    navBar

--- Shoping-clothes/src/App.jsx
+++ Shoping-clothes/src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
-import ContextProvider from './Context/ContextCarrito';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'; 
 import Landing from './Views/Landing/Landing';
 import Registro from './Views/Registro';
+import NavBar from './Components/NavBar';
+import Carrito from './Views/Carrito';
+import Home from './Views/Home';
+import { useLocation } from 'react-router-dom';
+
 function App() {
+
+  const location = useLocation();
+
+  const shouldShowNavBar = location.pathname !== '/form';
   return (
-    <ContextProvider>
-      <Router> 
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path='/form' element = {<Registro/>}/>
-        </Routes>
-      </Router>
-    </ContextProvider>
+    
+    <div>
+    {shouldShowNavBar && <NavBar />}
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path='/form' element={<Registro />} />
+      <Route path='/Home' element={<Home />} />
+      <Route path='/Carrito' element={<Carrito />} />
+    </Routes>
+  </div>
+    
   )
 }
 

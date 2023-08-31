b31b364595ba4be7aebebac6791596da0d1ff7de
goku673 <zapatamorato@gmail.com>
2023-08-31 07:55:12 -0400

    formulario mejorado

--- Shoping-clothes/src/Views/Home.jsx
+++ Shoping-clothes/src/Views/Home.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopingContext } from "../Context/ContextShoping";
 
 const Home = () => {
+   const {shoping, setShoping} = useContext(ShopingContext);
+    console.log(shoping);
     return (
-        <div>
-            <p>hola</p>
+        <div className="bg-red-400">
+          Hola
         </div>
     )
 }

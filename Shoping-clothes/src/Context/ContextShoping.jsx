a98e1828e006eef81a51d00fd8a547b31abf73b7
goku673 <zapatamorato@gmail.com>
2023-09-05 01:38:04 -0400

    todos los nuevos cambios y filtros

--- Shoping-clothes/src/Context/ContextShoping.jsx
+++ Shoping-clothes/src/Context/ContextShoping.jsx
@@ -3,29 +3,33 @@ import axios from "axios";
 
 export const ShopingContext = createContext();
 
-const ProveedorShoping = (props) => {
-      const [shoping, setShoping] = useState([]);
-      const [categorias ,setCategorias] =useState([]);
-      useEffect (() => {
-         const traerData = async () => {
-             const response = await axios.get('https://fakestoreapi.com/products');
-             const data = response.data;
-             setShoping(data);
-         }
-         const traerCategorias  = async() => {
-             const response = await axios.get('https://fakestoreapi.com/products/categories');
-             const data = response.data
-             setCategorias(data);
-         }
-         traerData();
-         traerCategorias();
-      },[]);
+const ShoppingProvider = (props) => {
 
-      return (
-        <ShopingContext.Provider value={{shoping,setShoping,categorias,setCategorias}}>
-          {props.children}
-        </ShopingContext.Provider>
-      );
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      setProducts(response.data);
+    }
+    
+    const getCategories = async () => {
+      const response = await axios.get('https://fakestoreapi.com/products/categories');
+      setCategories(response.data);
+    }
+
+    getProducts();
+    getCategories();
+     
+    
+  }, []);
+
+  return (
+    <ShopingContext.Provider value={{products, categories}}>
+      {props.children}  
+    </ShopingContext.Provider>
+  );
 }
 
-export default ProveedorShoping;
+export default ShoppingProvider;
